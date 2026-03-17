12258810c24f8cb86f6b198fbc54a87b1913bbf0
Trillion_Impulse <tkdduq2897@naver.com>
2026-03-17 16:45:08 +0900

    feat(vectorize): 텍스트 리스트를 숫자 벡터로 변환하는 함수 추가

--- src/vectorize.py
+++ src/vectorize.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from typing import List
+from scipy.sparse import csr_matrix
 
 def create_vectorizer() -> TfidfVectorizer:
     """
@@ -19,3 +20,13 @@ def fit_vectorizer(vectorizer: TfidfVectorizer, texts: List[str]) -> TfidfVector
     vectorizer.fit(texts)
 
     return vectorizer
+
+
+def transform_text(vectorizer: TfidfVectorizer, texts: List[str]) -> csr_matrix:
+    """
+    텍스트 리스트를 숫자 벡터로 변환
+    """
+
+    vectors = vectorizer.transform(texts)
+
+    return vectors
